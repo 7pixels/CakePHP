748740b27ec49bffd4a45b0b3d01f6701e52803f
Laud Tetteh <laud@moz.com>
2016-09-05 05:09:05 -0700

    Make "Title" a link to specific view + clean up TopicsController spacing

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -11,16 +11,11 @@
 
 		public function add() {
 			if($this->request->is('post')) {
-
 				$this->Topic->Create();
-
 				if($this->Topic->save($this->request->data)) {
 					$this->Flash->set('The topic has been created!');
 					$this->redirect('index');
 				}
-
-				
 			}
-
 		}
 	}
